e929a13d60c703b29681021a8e97512cf567ba85
Taylor Phelps <tphelps1993@gmail.com>
2020-11-04 15:40:20 -0500

    heroku ready

---
+++ src/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
+  DATABASE_URL:
+    process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/bookmarks',
+};
